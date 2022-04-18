b1e3716bc25696504465aeadf0dd1642540ce7b0
Igor Volokho <IgorVolokho99@gmail.com>
2022-04-18 03:20:01 +0300

    Second commit.Add person

--- stickmangame.py
+++ stickmangame.py
@@ -2,15 +2,34 @@ from tkinter import *
 import random
 import time
 
-class Sprite:
-    def __init__(self, game):
-        self.game = game
-        self.endgame = False
-        self.coordinates = None
-    def move(self):
-        pass
-    def coords(self):
-        return self.coordinates
+class Game:
+    def __init__(self):
+        self.tk = Tk()
+        self.tk.title("Человечек спешит к выходу")
+        self.tk.resizable(0, 0)
+        self.tk.wm_attributes("-topmost", 1)
+        self.canvas = Canvas(self.tk, width=500, height=500, highlightthickness=0)
+        self.canvas.pack()
+        self.tk.update()
+        self.canvas_height = 500
+        self.canvas_width = 500
+        self.bg = PhotoImage(file="background.gif")
+        w = self.bg.width()
+        h = self.bg.height()
+        for x in range(0, 5):
+            for y in range(0, 5):
+                self.canvas.create_image(x * w, y * h, image=self.bg, anchor='nw')
+        self.sprites = []
+        self.running = True
+
+    def mainloop(self):
+        while 1:
+            if self.running == True:
+                for sprite in self.sprites:
+                    sprite.move()
+            self.tk.update_idletasks()
+            self.tk.update()
+            time.sleep(0.01)
 
 class Coords:
     def __init__(self, x1, y1, x2, y2):
@@ -18,14 +37,7 @@ class Coords:
         self.y1 = y1
         self.x2 = x2
         self.y1 = y2
-
-class PlatformSprite(Sprite):
-    def __init__(self, game, photo_image, x, y, width, height):
-        Sprite.__init__(self, game)
-        self.photo_image = photo_image
-        self.image = game.canvas.create_image(x, y, image=self.photo_image, anchor='nw')
-        self.coordinates = Coords(x, y, x + width, y + height)
-
+    
 def within_x(co1, co2):
         if co1.x1 > co2.x1 and co1.x1 < co2.x2:
             return True
@@ -76,35 +88,62 @@ def collided_bottom(y, co1, co2):
             return True
     return False
 
+class Sprite:
+    def __init__(self, game):
+        self.game = game
+        self.endgame = False
+        self.coordinates = None
+    def move(self):
+        pass
+    def coords(self):
+        return self.coordinates
 
-class Game:
-    def __init__(self):
-        self.tk = Tk()
-        self.tk.title("Человечек спешит к выходу")
-        self.tk.resizable(0, 0)
-        self.tk.wm_attributes("-topmost", 1)
-        self.canvas = Canvas(self.tk, width=500, height=500, highlightthickness=0)
-        self.canvas.pack()
-        self.tk.update()
-        self.canvas_height = 500
-        self.canvas_width = 500
-        self.bg = PhotoImage(file="background.gif")
-        w = self.bg.width()
-        h = self.bg.height()
-        for x in range(0, 5):
-            for y in range(0, 5):
-                self.canvas.create_image(x * w, y * h, image=self.bg, anchor='nw')
-        self.sprites = []
-        self.running = True
+class PlatformSprite(Sprite):
+    def __init__(self, game, photo_image, x, y, width, height):
+        Sprite.__init__(self, game)
+        self.photo_image = photo_image
+        self.image = game.canvas.create_image(x, y, image=self.photo_image, anchor='nw')
+        self.coordinates = Coords(x, y, x + width, y + height)
 
-    def mainloop(self):
-        while 1:
-            if self.running == True:
-                for sprite in self.sprites:
-                    sprite.move()
-            self.tk.update_idletasks()
-            self.tk.update()
-            time.sleep(0.01)
+class StickFigureSprite(Sprite):
+    def __init__(self, game):
+        Sprite.__init__(self, game)
+        self.image_left = [
+            PhotoIgame(file="figure-L1.gif"),
+            PhotoIgame(file="figure-L2.gif"),
+            PhotoIgame(file="figure-L3.gif"),
+        ]
+        self.image_right = [
+            PhotoIgame(file="figure-R1.gif"),
+            PhotoIgame(file="figure-R2.gif"),
+            PhotoIgame(file="figure-R3.gif"),
+        ]
+        self.image = game.canvas.create_imgae(200, 470, image=self.images_left[0], anchor='nw')
+        self.x = -2
+        self.y = 0
+        self.current_image = 0
+        self.current_image_add = 1
+        self.jump_count = 0
+        self.last_time = time.time()
+        self.coordinates = Coords()
+        game.canvas.bind_all('<KeyPress-Left>', self.turn_left)
+        game.canvas.bind_all('<KeyPress-Right>', self.turn_right)
+        game.canvas.bind_all('<space>', self.jump)
+
+    def turn_left(self, evt):
+        if self.y == 0:
+            self.x = -2
+
+    def turn_right(self, evt):
+        if self.y == 0:
+            self.x = 2
+
+    def jump(self, evt):
+        if self.y == 0:
+            self.y = -4
+            self.jump_count = 0
+
+
 
 
 g = Game()
